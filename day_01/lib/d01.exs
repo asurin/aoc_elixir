a7a08dfc0b9d5133a3a0ff1cf66d68fcc1fde073
Alastair Surin <alastairjsurin@gmail.com>
2019-09-03 21:12:05 -0400

    Part one

--- day_01/lib/d01.exs
+++ day_01/lib/d01.exs
@@ -1,22 +1,22 @@
 defmodule D01 do
 
-  def find_final_frequency(input, start \\ 0) do
-    [Integer.to_string(start) | input]
-    |> IO.inspect()
-    |> Enum.map(&(String.replace_prefix(&1, "+", "")))
-    |> Enum.map(&(String.to_integer(&1)))
-    |> IO.inspect
+  def find_final_frequency(input) do
+    input
+    |> input_to_integers()
     |> Enum.sum()
   end
 
-  def 
-
-  def find_repeating_frequency(input, last_frequency \\ 0, frequencies \\ %{}) do
-    new_freq = find_final_frequency(input, last_frequency)
-    if frequencies[last_frequency] do
+  def find_repeating_frequency(input) do
+    integers = input_to_integers(input)
+    find_repeating_frequency(integers, 0, integers, %{})
+  end
+  defp find_repeating_frequency([], current_frequency, frequency_list, %{} = frequencies), do: find_repeating_frequency(frequency_list, current_frequency, frequency_list, frequencies)
+  defp find_repeating_frequency([item | remaining_items], current_frequency, frequency_list, %{} = frequencies) do
+    new_freq = current_frequency + item
+    if frequencies[new_freq] do
       new_freq
     else
-      find_repeating_frequency(input, new_freq, Map.put(frequencies, last_frequency, true))
+      find_repeating_frequency(remaining_items, new_freq, frequency_list, Map.put(frequencies, current_frequency, true))
     end
   end
 
@@ -26,11 +26,21 @@ defmodule D01 do
     |> String.split("\n")
   end
 
+  defp input_to_integers(input) do
+    input
+    |> Enum.map(&(String.replace_prefix(&1, "+", "")))
+    |> Enum.map(&(String.to_integer(&1)))
+  end
 end
 
-if Mix.env != :test do
-  "input.txt"
-  |> D01.load_file()
-  |> D01.find_final_frequency()
-  |> IO.inspect()
-end
+IO.puts("Part One:")
+"input.txt"
+|> D01.load_file()
+|> D01.find_final_frequency()
+|> IO.inspect()
+
+IO.puts("Part Two:")
+"input.txt"
+|> D01.load_file()
+|> D01.find_repeating_frequency()
+|> IO.inspect()
